7ea30bf192f1dd2afe125ac7359a593ddc3a03b6
Andrew Bautista <andrewbautista85@gmail.com>
2020-06-28 16:11:03 -0400

    adjusted topbar component with an index.js and style.css file, added color gradient

---
+++ src/components/TopBar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css'
+import { Container } from 'react-bootstrap';
+
+function TopBar() {
+
+    return (
+        <Container className="topBar">
+            <div>Ginny GreyBill
+        </div>
+        </Container>
+    )
+}
+
+export default TopBar;
