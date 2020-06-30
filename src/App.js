e3dc935684a980bc324c7ec095c75b38fb395a6c
Andrew Bautista <andrewbautista85@gmail.com>
2020-06-30 16:42:21 -0400

    added position fixed for topbar and sidebar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import TopBar from './components/TopBar';
 import SideBar from './components/SideBar';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
     <div>
       <TopBar />
       <SideBar />
+      <MainContent />
     </div>
   );
 }
--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import './style.css';
+import { Container, Row } from 'react-bootstrap';
 
 function MainContent() {
 
+    return (
+        <Container fluid>
+            <Row>
+                <h1>Graphic Design</h1>
+                <hr />
+            </Row>
+            <Row>
+                <h1>Graphic Design</h1>
+                <hr />
+            </Row>
+            <Row>
+                <h1>Graphic Design</h1>
+                <hr />
+            </Row>
+            <Row>
+                <h1>Graphic Design</h1>
+                <hr />
+            </Row>
+            <Row>
+                <h1>Graphic Design</h1>
+                <hr />
+            </Row>
+        </Container>
+    )
+
 };
 
-export default MainContent();
+export default MainContent;
