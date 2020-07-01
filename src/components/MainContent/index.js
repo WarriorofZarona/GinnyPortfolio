2fd9483ee6a6c0fce7bc958d051bf3c120a622d0
Andrew Bautista <andrewbautista85@gmail.com>
2020-07-01 18:03:04 -0400

    coded main content with 2 columns of 8, as well as portrait/text example

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -1,38 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css'
 
 function MainContent() {
 
+    const [image, setImage] = useState({
+        src: "https://via.placeholder.com/350x500?text=portrait",
+        orientation: "portrait",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a neque lectus. Nunc euismod velit convallis, hendrerit enim quis, porttitor est. Aliquam et luctus metus. Morbi in tristique lorem. Maecenas ullamcorper quam lectus, id molestie nunc tincidunt quis. Nulla facilisi. Maecenas vitae vehicula diam, eu fermentum nunc. Aliquam erat volutpat."
+    });
+
     return (
-        <Container fluid="md" className="main-content">
+        <Container fluid className="gallery">
             <Row>
-                <h1>Graphic Design</h1>
-                <hr />
+                <Col md={3}>
+                    <Container id="graphic-design">
+                        <Row>
+                            <Col><img className="thumbnail" src="https://via.placeholder.com/100" /></Col>
+                            <Col><img className="thumbnail" src="https://via.placeholder.com/100" /></Col>
+                        </Row>
+                        <Row>
+                            <Col><img className="thumbnail" src="https://via.placeholder.com/100" /></Col>
+                            <Col><img className="thumbnail" src="https://via.placeholder.com/100" /></Col>
+                        </Row>
+                        <Row>
+                            <Col><img className="thumbnail" src="https://via.placeholder.com/100" /></Col>
+                            <Col><img className="thumbnail" src="https://via.placeholder.com/100" /></Col>
+                        </Row>
+                        <Row>
+                            <Col><img className="thumbnail" src="https://via.placeholder.com/100" /></Col>
+                            <Col><img className="thumbnail" src="https://via.placeholder.com/100" /></Col>
+                        </Row>
+                    </Container>
+                </Col>
+
+                <Col>
+                    {image.orientation === "portrait" ?
+                        <Container fluid className="focused-image">
+                            <Row>
+                                <Col><img src={image.src} /></Col>
+                                <Col> <h4>{image.text}</h4></Col>
+                            </Row>
+                        </Container>
+                        :
+                        <Container fluid className="focused-image">
+                            <Row>
+                                <img src={image.src} />
+                            </Row>
+                            <Row>
+                                <h4>{image.text}</h4>
+                            </Row>
+                        </Container>
+                    }
+                </Col>
             </Row>
-            <Row>
-                <Col><img src="https://via.placeholder.com/150" /></Col>
-                <Col><img src="https://via.placeholder.com/150" /></Col>
-                <Col><img src="https://via.placeholder.com/150" /></Col>
-            </Row>
-            <Row>
-                <h1>Graphic Design</h1>
-                <hr />
-            </Row>
-            <Row>
-                <h1>Graphic Design</h1>
-                <hr />
-            </Row>
-            <Row>
-                <h1>Graphic Design</h1>
-                <hr />
-            </Row>
-            <Row>
-                <h1>Graphic Design</h1>
-                <hr />
-            </Row>
-        </Container>
+        </Container >
     )
 
 };
--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './style.css'
-import { Navbar, Col } from 'react-bootstrap';
+import { Navbar, Col, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function TopBar() {
 
     return (
         <Navbar className="top-bar fixed-top">
-            <Col md="auto" className="logo">Ginny Graybill
-        </Col>
+            <Row>
+                <Col md="auto" className="logo">Ginny Graybill</Col>
+            </Row>
         </Navbar>
     )
 }
