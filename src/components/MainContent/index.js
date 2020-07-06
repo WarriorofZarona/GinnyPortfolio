ff5ed68ade6882b2ccc04a3229b071af30f258d3
Andrew Bautista <andrewbautista85@gmail.com>
2020-07-06 15:20:41 -0400

    testing out sample portrait and landscape images and css coding successful

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import './style.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css'
+import Portrait from '../../assets/images/1.jpg'
+import Landscape from '../../assets/images/VirginiaGraybill_BeercanMockup2.jpg'
 
 function MainContent() {
 
     const [image, setImage] = useState({
-        src: "https://via.placeholder.com/350x470?text=portrait",
-        orientation: "portrait",
+        src: Landscape,
+        orientation: "landscape",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a neque lectus. Nunc euismod velit convallis, hendrerit enim quis, porttitor est. Aliquam et luctus metus. Morbi in tristique lorem. Maecenas ullamcorper quam lectus, id molestie nunc tincidunt quis. Nulla facilisi. Maecenas vitae vehicula diam, eu fermentum nunc. Aliquam erat volutpat."
     });
 
@@ -37,16 +39,16 @@ function MainContent() {
 
                 <Col md={9}>
                     {image.orientation === "portrait" ?
-                        <Container fluid className="focused-image">
+                        <Container fluid>
                             <Row>
-                                <Col><img src={image.src} /></Col>
+                                <Col><img className="portrait" src={image.src} /></Col>
                                 <Col> <h4>{image.text}</h4></Col>
                             </Row>
                         </Container>
                         :
-                        <Container fluid className="focused-image">
+                        <Container fluid>
                             <Row>
-                                <img src={image.src} />
+                                <img className="landscape" src={image.src} />
                             </Row>
                             <Row>
                                 <h4>{image.text}</h4>
