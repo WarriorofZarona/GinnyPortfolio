ecd51f73c17b8baad5e219629b6ab9ca74717554
Andrew Bautista <andrewbautista85@gmail.com>
2020-07-06 15:34:37 -0400

    added utils that exports array of images with category, orientation, and text

---
+++ src/utils/db.js
@@ -0,0 +1,72 @@
+import {
+    portrait1,
+    portrait2,
+    portrait3,
+    portrait4,
+    portrait5,
+    portrait6,
+    portrait7,
+    portrait8,
+    landscape1
+} from '../assets/images/'
+
+export const images = [{
+    src: portrait1,
+    orientation: 'portrait',
+    category: 'digital artwork',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula, quam a blandit porta, sem neque accumsan ipsum, ut varius elit nulla varius ligula. Curabitur ullamcorper massa quis metus euismod imperdiet. Sed ac consequat metus. Vestibulum et iaculis ipsum. Nullam aliquet condimentum ex nec aliquam.'
+
+},
+{
+    src: portrait2,
+    orientation: 'portrait',
+    category: 'digital artwork',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula, quam a blandit porta, sem neque accumsan ipsum, ut varius elit nulla varius ligula. Curabitur ullamcorper massa quis metus euismod imperdiet. Sed ac consequat metus. Vestibulum et iaculis ipsum. Nullam aliquet condimentum ex nec aliquam.'
+
+},
+{
+    src: portrait3,
+    orientation: 'portrait',
+    category: 'digital artwork',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula, quam a blandit porta, sem neque accumsan ipsum, ut varius elit nulla varius ligula. Curabitur ullamcorper massa quis metus euismod imperdiet. Sed ac consequat metus. Vestibulum et iaculis ipsum. Nullam aliquet condimentum ex nec aliquam.'
+},
+{
+    src: portrait4,
+    orientation: 'portrait',
+    category: 'digital artwork',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula, quam a blandit porta, sem neque accumsan ipsum, ut varius elit nulla varius ligula. Curabitur ullamcorper massa quis metus euismod imperdiet. Sed ac consequat metus. Vestibulum et iaculis ipsum. Nullam aliquet condimentum ex nec aliquam.'
+
+},
+{
+    src: portrait5,
+    orientation: 'portrait',
+    category: 'digital artwork',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula, quam a blandit porta, sem neque accumsan ipsum, ut varius elit nulla varius ligula. Curabitur ullamcorper massa quis metus euismod imperdiet. Sed ac consequat metus. Vestibulum et iaculis ipsum. Nullam aliquet condimentum ex nec aliquam.'
+
+},
+{
+    src: portrait6,
+    orientation: 'portrait',
+    category: 'digital artwork',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula, quam a blandit porta, sem neque accumsan ipsum, ut varius elit nulla varius ligula. Curabitur ullamcorper massa quis metus euismod imperdiet. Sed ac consequat metus. Vestibulum et iaculis ipsum. Nullam aliquet condimentum ex nec aliquam.'
+},
+{
+    src: portrait7,
+    orientation: 'portrait',
+    category: 'digital artwork',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula, quam a blandit porta, sem neque accumsan ipsum, ut varius elit nulla varius ligula. Curabitur ullamcorper massa quis metus euismod imperdiet. Sed ac consequat metus. Vestibulum et iaculis ipsum. Nullam aliquet condimentum ex nec aliquam.'
+
+},
+{
+    src: portrait8,
+    orientation: 'portrait',
+    category: 'graphic design',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula, quam a blandit porta, sem neque accumsan ipsum, ut varius elit nulla varius ligula. Curabitur ullamcorper massa quis metus euismod imperdiet. Sed ac consequat metus. Vestibulum et iaculis ipsum. Nullam aliquet condimentum ex nec aliquam.'
+
+},
+{
+    src: landscape1,
+    orientation: 'landscape',
+    category: 'graphic design',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula, quam a blandit porta, sem neque accumsan ipsum, ut varius elit nulla varius ligula. Curabitur ullamcorper massa quis metus euismod imperdiet. Sed ac consequat metus. Vestibulum et iaculis ipsum. Nullam aliquet condimentum ex nec aliquam.'
+}];
