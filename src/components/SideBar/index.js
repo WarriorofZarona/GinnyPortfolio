4d95815cc68839f5e74416c172024e08be4e3c44
Andrew Bautista <andrewbautista85@gmail.com>
2020-07-07 12:57:11 -0400

    added nav item codes

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,22 +1,48 @@
 import React from 'react';
 import './style.css'
-import { Nav } from 'react-bootstrap';
+import { Nav, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function SideBar() {
 
     return (
         <Nav fluid className='col-md-12 d-none d-md-block sidebar'>
-            <div className="nav">Graphic Design</div>
-            <div className="nav">Digital Artwork</div>
-            <div className="nav">Paintings</div>
-            <div className="nav">Portraits</div>
-            <div className="nav">Emotes</div>
-            <div className="nav">About Me</div>
-            <div className="nav">Contact</div>
-            <i className="fab fa-instagram icon-item"></i>
-            <i className="fab fa-twitter icon-item"></i>
-            <i className="fab fa-behance icon-item"></i>
+
+
+            <Nav.Item className='nav-item'>
+                <Nav.Link href='/graphic-design'>
+                    Graphic Design</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className='nav-item'>
+                <Nav.Link href='/digital-artwork'>
+                    Digital Artwork</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className='nav-item'>
+                <Nav.Link href='/paintings'>
+                    Paintings</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className='nav-item'>
+                <Nav.Link href='/portraits'>
+                    Portraits</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className='nav-item'>
+                <Nav.Link href='/emotes'>
+                    Emotes</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className='nav-item'>
+                <Nav.Link href='/about-me'>
+                    About Me</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className='nav-item'>
+                <Nav.Link href='/contact'>
+                    Contact</Nav.Link>
+            </Nav.Item>
+
+            <Row className='icons'>
+                <i className="fab fa-instagram icon-item"></i>
+                <i className="fab fa-twitter icon-item"></i>
+                <i className="fab fa-behance icon-item"></i>
+            </Row>
         </Nav >
     )
 }
