98a7ddc3d53b1d02600ff832322c7119fddc58f4
Andrew Bautista <andrewbautista85@gmail.com>
2021-02-16 10:55:47 -0500

    made adjustments to custom css if it affects the deployed

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { Navbar, Col, Row } from 'react-bootstrap';
-import './style.css'
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import { Navbar, Col, Row } from "react-bootstrap";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function TopBar() {
-
-    return (
-        <Navbar className="top-bar fixed-top">
-            <Row>
-                <Col id="logo" md="auto">Ginny Graybill</Col>
-            </Row>
-        </Navbar>
-    )
+  return (
+    <Navbar className="top-bar fixed-top">
+      <Row>
+        <Col sm="auto" className="logo">
+          Ginny Graybill
+        </Col>
+      </Row>
+    </Navbar>
+  );
 }
 
 export default TopBar;
